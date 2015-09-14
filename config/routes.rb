7e31bdf61177140fb9c8de076d0cd29ad6e00468
Jay Snee <jay.snee@me.com>
2015-09-14 16:44:04 +0100

    rootin'

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   
   mount Sidekiq::Web => '/sidekiq'
-  root 'systems#index'
+  root 'requests#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
